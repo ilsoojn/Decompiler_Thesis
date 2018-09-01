486fe5c7280c96917003acf7d8dcc359d1872d15
Ilsoo Jeon <bunnymonsieur92@gmail.com>
2018-09-01 18:36:04 +0000

    End of Aug

--- Elimination.hs
+++ Elimination.hs
@@ -19,110 +19,130 @@ import StatementParse
 import StatementInstr
 import OtherFunction
 
-propagation :: [String] -> String -> Integer -> [String] -> [String]
-propagation [] fname n preCont = preCont
-propagation (line : nextCont) fname n preCont
-  | (isFunction line) = propagation nextCont (getFunctionName line) (n + 1) (preCont ++ [line])
+isVarDeclare line fname = (not.isFunction) line && (not.isBlock) line && (not.null) fname && (isInfixOf " = " line)
 
-  | ((not.isFunction) line && (not.isBlock) line && (not.null) fname && (isInfixOf " = " line)) = do
+propagation :: [String] -> String -> [String] -> [(String, String)] -> ([String], [(String, String)])
+propagation [] fname preCont vSet = (preCont, vSet)
+propagation (line:nextCont) fname preCont vSet
+  | (nextCont == [""]) = preCont
+  -- Function line
+  | (isFunction line) = propagation nextCont (getFunctionName line) (preCont ++ [line]) vSet
+  -- LHS line
+  | (isVarDeclare line fname) = do
+
     let x = statement (strip line) fname
         v = fromJust (fst x)
-        (v_var, v_reg) = snd x
-        use = findUse v nextCont fname (n + 1) []
-    if (isConv v_var && (not.null) use && is_useConv use)
+        (var, reg) = snd x
+        use = findUse v nextCont fname []
+
+        (pre_v, (pre_var, pre_reg)) = statement (last preCont) fname
+        (next_v, (next_var, next_reg)) =  statement (head nextCont) fname
+
+    -- condition (def: cond, use: cond)
+    if (isConv var && (not.null) use && is_useConv use)
       then do
-        let from_1 = (ty2 v_var) ++ " " ++ v ++ " "
-            to_1 = (ty1 v_var) ++ " " ++ (value v_var) ++ " "
+        let from_1 = (ty var) ++ " " ++ v ++ " "
+            to_1 = (ty1 var) ++ " " ++ (value var) ++ " "
             tmp_nextCont = lines $ replace from_1 to_1 (unlines nextCont)
 
-            from_2 = (ty2 v_var) ++ " " ++ v ++ ","
-            to_2 = (ty1 v_var) ++ " " ++ (value v_var) ++ ","
+            from_2 = (ty var) ++ " " ++ v ++ ","
+            to_2 = (ty1 var) ++ " " ++ (value var) ++ ","
             new_nextCont = lines $ replace from_2 to_2 (unlines tmp_nextCont)
-        propagation new_nextCont fname n preCont  -- n
+        propagation new_nextCont fname preCont vSet
 
-      else if (isBitwise v_var)
+      -- idiom 1: OR (ZEXT a) (AND x 0s)
+      else if (isBitwise var)
         then do
-          let constInt = map round (map read (map strip $ getConstantValues v_reg) :: [Double])
+          let constInt = map round (map read (map strip $ getConstantValues reg) :: [Double])
               (is_bit0s, is_1s) = (hasZeros constInt, hasOnes constInt)
               is_num0s = (0 == head constInt) || (0 == last constInt)
-          case (op v_var, is_bit0s, is_num0s) of --, is_1s) of
+          case (op var, is_bit0s, is_num0s) of --, is_1s) of
             ("and", True, False) -> do -- > 0s
-              let (pre_v, (pre_var, pre_reg)) = statement (last preCont) fname
-                  (next_v, (next_var, next_reg)) =  statement (head nextCont) fname
 
               if ((op pre_var) == "zext" && (op next_var) == "or" && next_reg == [fromJust pre_v, v])
                 then do
-                  let newLine = (fromJust next_v) ++ " = " ++ v ++ ":" ++ (fromJust pre_v)
-                  propagation (tail nextCont) fname (n - 1) (init preCont ++ [newLine]) -- n
+                  --let newLine = (fromJust next_v) ++ " = " ++ (head reg) ++ " : " ++ (head pre_reg)
+                  -- trace("prop: "++ (fromJust next_v) ++ " = " ++ (head reg) ++ " : " ++ (head pre_reg) ++ "\n\t"++(last preCont) ++ "\n\t" ++ line ++ "\n\t" ++ (head nextCont))
+                  let newLine = (fromJust next_v) ++ " = " ++ (head pre_reg)
+                  propagation (tail nextCont) fname (init preCont ++ [newLine]) vSet
 
-                else propagation nextCont fname (n + 1) (preCont ++ [line])
+                else propagation nextCont fname (preCont ++ [line]) vSet
             -- ("and", False, True) -> ["and 1s"]  -- > x
             -- ("or", True, False) -> ["or 0s"]    -- > x
             -- ("or", False, True) -> ["or 1s"]    -- > 1s
-            _ -> propagation nextCont fname (n + 1) (preCont ++ [line])
-        else propagation nextCont fname (n + 1) (preCont ++ [line])
-  | otherwise = propagation nextCont fname (n + 1) (preCont ++ [line])
+            _ -> propagation nextCont fname (preCont ++ [line]) vSet
 
-elimination :: [String] -> String -> Integer -> [String] -> [String]
-elimination [] fname n preCont = preCont
-elimination (line : nextCont) fname n preCont
-  | (isFunction line) = elimination nextCont (getFunctionName line) (n + 1) (preCont ++ [line])
-  | ((not.isFunction) line && (not.isBlock) line && (not.null) fname && (isInfixOf " = " line)) = do
+        else if (isBinary var && isStackPtr line && isConv next_var)
+          then do
+            let (rsp, idx) = (head reg, read (last reg) :: Integer)
+            case (op var, op next_var, idx < 0) of
+              ("add", "inttoptr", True) -> do
+                let new_nextCont = usePropagation (fromJust next_v) (rsp ++ (show idx)) (tail nextCont)
+                propagation new_nextCont fname preCont vSet
+              ("sub","inttoptr", True) -> do
+                let new_nextCont = usePropagation (fromJust next_v) (rsp ++ "+" ++ (show $ abs idx)) (tail nextCont)
+                propagation new_nextCont fname preCont vSet
+              ("add","inttoptr", False) -> do
+                let new_nextCont = usePropagation (fromJust next_v) (rsp ++ "+" ++ (show idx)) (tail nextCont)
+                propagation new_nextCont fname preCont vSet
+              ("sub","inttoptr", False) -> do
+                let new_nextCont = usePropagation (fromJust next_v) (rsp ++ "-" ++ (show idx)) (tail nextCont)
+                propagation new_nextCont fname preCont vSet
+              --   let newLine = (fromJust next_v) ++ " = " ++ rsp ++ "+" ++ (show (abs idx))
+              --   propagation (tail nextCont) fname (n + 1) (init preCont ++ [newLine])
+              _ -> propagation nextCont fname (preCont ++ [line]) vSet
+          else propagation nextCont fname (preCont ++ [line]) vSet
+
+  | otherwise = propagation nextCont fname (preCont ++ [line]) vSet
+
+-- extensionTrim :: [String] -> String -> Integer -> [String] -> [String]
+-- extensionTrim [] fn n preCont = preCont
+-- extensionTrim (line : nextCont) fname n preCont
+--   | var_x && dec_x = do
+--     let (vary, y) = statement (strip (head nextCont)) fname
+--         (varz, z) = statement (strip (head $ tail nextCont)) fname
+--
+--     if (isDeclare $ fst y) && (isDeclare $ fst z)
+--       then do
+--         let v = low (fst x)
+--             new_nextCont = trace(":"++fromJust varx ++ " -> " ++ v) rp (fromJust varx) v nextCont
+--         extensionTrim new_nextCont fname n preCont
+--
+--       else extensionTrim nextCont fname (n + 1) (preCont ++ [line])
+--   | otherwise = extensionTrim nextCont fname (n + 1) (preCont ++ [line])
+--   where (varx, x) = statement (strip line) fname
+--         var_x = isVarDeclare line fname
+--         dec_x = isDeclare (fst x)
+--         strx = isNothing varx
+
+elimination :: [String] -> String -> [String] -> [String]
+elimination [] fname preCont = preCont
+elimination (line : nextCont) fname preCont
+  | (isFunction line) = elimination nextCont (getFunctionName line) (preCont ++ [line])
+  | (isVarDeclare line fname) = do
     let x = statement (strip line) fname
         v = fromJust (fst x)
-        (v_var, v_reg) = snd x
-        use = findUse v nextCont fname (n + 1) []
+        use = findUse v nextCont fname []
 
     if (null use)
-      then elimination nextCont fname n preCont
-      else elimination nextCont fname (n + 1) (preCont ++ [line])
-  | otherwise = elimination nextCont fname (n + 1) (preCont ++ [line])
+      then elimination nextCont fname preCont
+      else if (isAequalB line)
+        then do
+          let u = '%': (getB line)
+              new_nextCont = rp v u nextCont
+          elimination new_nextCont fname preCont
+        else elimination nextCont fname (preCont ++ [line])
+  | otherwise = elimination nextCont fname (preCont ++ [line])
 
--- elimination :: [String] -> String -> Integer -> [String] -> [String]
--- elimination [] fname n preCont = preCont
--- elimination (line : nextCont) fname n preCont
---   | (null fname && (not $ isFunction line)) = trace(" "++ (show n) ++ "\t0 & none >> " ++ line) elimination nextCont fname (n+1) preCont --n
---   | (isFunction line) = do
---     let fn = (getFunctionName line)
---     trace(" "++ (show n) ++ "\t1.0 & " ++ fn ++ " >> " ++ line) elimination nextCont (getFunctionName line) (n + 1) (preCont ++ [line])
---   | (isBlock line) = trace(" " ++ (show n) ++ "\t2.0 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n + 1) (preCont ++ [line])
---   | (isInfixOf " = " line) = do
---     let x = statement (strip line) fname
---         v = fromJust (fst x)
---         (v_var, v_reg) = snd x
---         use = findUse v nextCont fname (n + 1) []
---
---     if (length use == 0)
---       then trace(" " ++ (show n) ++ "\t3.1 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n+1) preCont  -- n
---
---       else if (isConv v_var && is_useConv use)
+-- createVar :: [String] -> String -> Integer -> [String] -> [String]
+-- createVar [] fname vc preCont = preCont
+-- createvar (line:nextCont) fname vc
+--   | (isFunction line) = creatVar nextCont (getFunctionName line) (n + 1) list
+--   | (isStackPtr line) = do
+--     let rsp_idx = convArith line fname
+--         e = (fname, rsp_idx)
+--     if (lookup e table)
 --         then do
---           let from = (ty2 v_var) ++ " " ++ v
---               to = (ty1 v_var) ++ " " ++ (value v_var)
---               new_nextCont = lines $ replace from to (unlines nextCont)
---           trace(" " ++ (show n) ++ "\t3.2 & " ++ fname ++ " >> " ++ line) elimination new_nextCont fname (n+1) preCont  -- n
---     --  else trace(">> here2")elimination nextCont fname (n + 1) (preCont ++ [line]) -- << TEST CASE
---         else if (isBinary v_var || isBitwise v_var)
---           then do
---             let constVal = (map strip $ getConstantValues v_reg)
---                 constInt = map round (map read constVal :: [Double])
---                 is_0s = hasZeros constInt
---                 is_1s = hasOnes constInt
---             case (op v_var, is_0s) of --, is_1s) of
---               ("and", True) -> do -- > 0s
---                 let (pre_v, (pre_var, pre_reg)) = statement (last preCont) fname
---                     (next_v, (next_var, next_reg)) =  statement (head nextCont) fname
---
---                 if ((op pre_var) == "zext" && (op next_var) == "or" && next_reg == [fromJust pre_v, v])
---                   then do
---                     let newLine = (fromJust next_v) ++ " = " ++ v ++ ":" ++ (fromJust pre_v)
---                     trace(" " ++ (show n) ++ "\t3.3 & " ++ fname ++ " >> " ++ line) elimination (tail nextCont) fname (n+1) (init preCont ++ [newLine]) -- n
---
---                   else trace(" " ++ (show n) ++ "\t3.4 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n + 1) (preCont ++ [line])
---               -- ("and", False, True) -> ["and 1s"]  -- > x
---               -- ("or", True, False) -> ["or 0s"]    -- > x
---               -- ("or", False, True) -> ["or 1s"]    -- > 1s
---               _ -> trace(" " ++ (show n) ++ "\t3.5 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n + 1) (preCont ++ [line])
---
---           else trace(" " ++ (show n) ++ "\t3.6 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n + 1) (preCont ++ [line])
---   | otherwise = trace(" " ++ (show n) ++ "\t4.0 & " ++ fname ++ " >> " ++ line) elimination nextCont fname (n + 1) (preCont ++ [line])
+--           let v = "%v" ++ (show vc)
+--               newList = list ++ []
+--   | otherwise = createVar nextCont fname (n + 1) list
